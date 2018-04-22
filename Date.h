00681ba4b15b97064f4937417ebefd79e9780d36
hiiideee <36652278+hiiideee@users.noreply.github.com>
2018-04-22 15:57:50 -0400

    Update Date.h

--- Date.h
+++ Date.h
@@ -1,15 +1,25 @@
+#pragma once
 #include <iostream>
 #include <string>
 
 class Date {
 
 private:
-  int Year, Month, Day;
+	int Year, Month, Day;
+	int t;
 
 public:
-  Date():
+	Date() : Year(0), Month(0), Day(0){}
+	Date(int year, int month, int day) : Year(year), Month(month), Day(day) { toT();} /// Maybe Wrong 
+	Date(const Date& date) : Year(date.Year), Month(date.Month), Day(date.Day) {}
+	~Date() {}
+	int getYear() const;
+	int getMonth() const;
+	int getDay() const;
+	void setYear(int year);
+	void setMonth(int month);
+	void setDay(int day);
+	std::string toString() const;
+	void toT();
+};
 
-
-
-
-}
