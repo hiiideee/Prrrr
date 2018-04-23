6b45f3399bfc2d0540fe4b39ddbd21db4d7a3afb
hiiideee <36652278+hiiideee@users.noreply.github.com>
2018-04-23 15:12:01 -0400

    Update Stock.h

--- Stock.h
+++ Stock.h
@@ -6,12 +6,18 @@
 #include <vector>
 #include "Date.h"
 
+struct CmpByKeyDate {
+	bool operator()(Date d1, Date d2) {
+		return d1.toT() < d2.toT();
+	}
+};
+
 class Stock {
 protected:
 	std::string Ticker;
 	Date Releasingdate;
-	std::map<Date, double> Prices;
-	std::map<Date, double> Returns;
+	std::map<Date, double, CmpByKeyDate> Prices;
+	std::map<Date, double, CmpByKeyDate> Returns;
 	double EPSbeat;
 
 public:
@@ -25,9 +31,9 @@ public:
 	const Date& getReleasingdate() const;
 	double getPrices(Date date) const;
 	double getReturns(Date date) const;
-	const std::map<Date, double>& GetPrices() const;
-	const std::map<Date, double>& GetReturns() const;
-	Stock slice(const Date &RleaseDate);
+	const std::map<Date, double, CmpByKeyDate>& GetPrices() const;
+	const std::map<Date, double, CmpByKeyDate>& GetReturns() const;
+	void slice(const Date &RleaseDate);
 	virtual void Display();
 };
 
@@ -39,7 +45,7 @@ public:
 	Information() {}
 	Information(std::string ticker, Date releasingdate, std::map<Date, double> prices, std::map<Date, double> returns, std::string address_, std::string sector_, std::string subindustry_);
 	Information(const Information& information);
-	~Information() {}
+	virtual ~Information() {}
 
 	std::string getAddress() const { return address; };
 	std::string getSector() const { return sector; };
