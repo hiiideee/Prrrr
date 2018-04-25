31c41d6542bbea3246c68b7c8e73ac2defb142de
hiiideee <36652278+hiiideee@users.noreply.github.com>
2018-04-25 15:53:24 -0400

    Update Stock.h

--- Stock.h
+++ Stock.h
@@ -21,6 +21,8 @@ protected:
 	std::map<Date, double, CmpByKeyDate> Prices;
 	std::map<Date, double, CmpByKeyDate> Returns;
 	double EPSestimate;
+	std::string StartTime;
+	std::string EndTime;
 	double EPSactual;
 
 public:
@@ -31,12 +33,14 @@ public:
 
 	double getMarketReturns(Date date) { auto it = MarketReturns.find(date); return it->second; };
 	std::string getTicker() const;
+	std::string getStartTime() { return StartTime; }
+	std::string getStartTime() { return EndTime; }
 	double getEPSbeat() const { return (EPSactual/EPSestimate) - 1; }
 	const Date& getReleasingdate() const { return Releasingdate; }
 	double getPrices(int t) { slice();  auto it = Prices.begin(); advance(it, t); return it->second; }
 	double getReturns(int t) { slice();  auto it = Returns.begin(); advance(it, t); return it->second; }
-	const std::map<Date, double, CmpByKeyDate>& GetPrices() const { return Prices; }
-	const std::map<Date, double, CmpByKeyDate>& GetReturns() const { return Returns; }
+	const std::map<Date, double, CmpByKeyDate> GetPrices() const { return Prices; }
+	const std::map<Date, double, CmpByKeyDate> GetReturns() const { return Returns; }
 	void slice();
 	virtual void Display();
 };
