f0310fc60f88cc1d43f552be296c720dc450409d
hiiideee <36652278+hiiideee@users.noreply.github.com>
2018-04-26 22:31:57 -0400

    Update Stock.cpp

--- Stock.cpp
+++ Stock.cpp
@@ -4,40 +4,50 @@
 #include <list>
 #include <vector>
 #include "Date.h"
-#include "TickerBook.h"
 #include "Stock.h"
 
 using namespace std;
+/*
+double Stock::getPrices(int t) {
+auto it = Prices.begin();
+advance(it, t);
+return it->second;
+}
 
-void Stock::slice() {
+double Stock::getReturns(int t) {
+auto it = Returns.begin();
+advance(it, t);
+return it->second;
+}
+*/
+void Stock::Display() {
+	std::cout << "Ticker: " << Ticker << std::endl;
+	std::cout << "Start date: " << StartTime << std::endl;
+	std::cout << "End date:" << EndTime << std::endl;
+	std::cout << "Estimated EPS:" << EPSestimate << std::endl;
+	std::cout << "Actual EPS:" << EPSactual << std::endl;
+	std::cout << "Prices:" << std::endl;
+	std::map<Date, double>::iterator itr;
+	for (itr = Prices.begin(); itr != Prices.end(); itr++)
+		std::cout << (itr->first).toString() << '    ' << itr->second;
+}
 
-	map<Date, double, CmpByKeyDate>::iterator it1;
-	map<Date, double, CmpByKeyDate>::iterator it2;
-	it1 = Prices.find(Releasingdate);
-	it2 = Prices.find(Releasingdate);
-	int c1 = 0, c2 = 0;
-	while (it1 != Prices.begin()) {
-		if (c1 >= 60) {
-			Prices.erase(it1--);
-			c1 ++;
-		}
-		else {
-			-- it1;
-			c1++;
-		}
+Market Market::slice(Date startdate, Date enddate) {
+	Market market(Ticker, startdate.toString(), enddate.toString(), EPSactual, EPSestimate);
+	for (auto i = Prices.find(startdate); i != ++(Prices.find(enddate)); i++)
+		market.Prices.insert(std::pair<Date, double>(i->first, i->second));
+	for (auto i = Returns.find(startdate); i != ++(Returns.find(enddate)); i++)
+		market.Returns.insert(std::pair<Date, double>(i->first, i->second));
+	return market;
+}
+
+void Stock::setReturns() {
+	auto it = Prices.begin();
+	for (it; it != Prices.end(); ) {
+		double lstday = it->second;
+		it++;
+		if (it == Prices.end()) break;
+		else Returns.insert(pair<Date, double>(it->first, (it->second - lstday) / lstday));
 	}
-
-	while (it2 != Prices.end()) {
-		if (c2 >= 60) {
-			Prices.erase(it2++);
-			c2++;
-		}
-		else {
-			++it2;
-			c2 ++;
-		}
-	}
-
-	if (Prices.size() != 121) cout << "wtf" << endl;
 }
 
