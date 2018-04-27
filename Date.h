a414136a8b2ca883dd13418e11ed75c82e02b02f
hiiideee <36652278+hiiideee@users.noreply.github.com>
2018-04-26 22:30:04 -0400

    Update Date.h

--- Date.h
+++ Date.h
@@ -9,10 +9,22 @@ private:
 	int t;
 
 public:
-	Date() : Year(0), Month(0), Day(0){}
-	Date(int year, int month, int day) : Year(year), Month(month), Day(day) { toT();} /// Maybe Wrong 
+	Date() : Year(0), Month(0), Day(0) {}
+	Date(int year, int month, int day) : Year(year), Month(month), Day(day) {}
+	Date(std::string date) /*:Year(stoi(date.substr(0, 4))), Month(stoi(date.substr(4, 2))), Day(stoi(date.substr(6, 2)))*/ {
+		Year = 2010;
+		Month = 12;
+		Day = 10;
+	};
 	Date(const Date& date) : Year(date.Year), Month(date.Month), Day(date.Day) {}
 	~Date() {}
+
+	bool operator>(const Date& obj) const;
+	bool operator<(const Date& obj) const;
+	bool operator==(const Date& obj) const;
+	bool operator<=(const Date& obj) const;
+	bool operator>=(const Date& obj) const;
+
 	int getYear() const;
 	int getMonth() const;
 	int getDay() const;
@@ -20,6 +32,5 @@ public:
 	void setMonth(int month);
 	void setDay(int day);
 	std::string toString() const;
-	void toT();
+	//int toT();
 };
-
