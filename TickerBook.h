0735c393f04155fd1569464539135986dfefb537
hiiideee <36652278+hiiideee@users.noreply.github.com>
2018-04-26 22:30:58 -0400

    Update TickerBook.h

--- TickerBook.h
+++ TickerBook.h
@@ -8,32 +8,25 @@
 #include "Stock.h"
 
 class TickerBook {
-protected:
-	std::map<std::string, Stock> Book;
-	std::list<std::string> BookPage;
+private:
 	int Size;
+	int RequiredSize;
 
 public:
-	TickerBook() {}
+	std::map<std::string, Stock> Book;
+	std::vector<std::string> BookPage;
+
+	TickerBook() { Size = 0; }
+	TickerBook(const TickerBook& tickerbook): Book(tickerbook.Book), BookPage(tickerbook.BookPage), Size(tickerbook.Size) {}
 	~TickerBook() {}
 
+
 	void setStock(const Stock& stock);
+	void setRequiredSize(int R) { RequiredSize = R; };
 	int getSize() const { return Size; };
 	void reorder();
+	std::vector<std::string> Divide(double threshold, int number);
+	void display();
 };
 
-class Group : TickerBook {
-private:
-	std::vector<std::vector<std::string>> map_keys; // three groups of ticker from tickerbook
-	std::vector<std::vector<double>> AAR = std::vector<std::vector<double>>(120);
-	std::vector<std::vector<double>> CAAR = std::vector<std::vector<double>>(120);
-
-public:
-	Group() {}
-	~Group() {}
-	void Divide(double threshold) const;
-	const std::vector<double>& GetAAR() const;
-	const std::vector<double>& GetCAAR() const;
-	bool Compute();
-};
 
