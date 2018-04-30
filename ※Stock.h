c793fd8658175facd58c0f151101bce29d5e6434
hiiideee <36652278+hiiideee@users.noreply.github.com>
2018-04-30 12:28:52 -0400

    Update and rename Stock.h to ※Stock.h

--- ※Stock.h
+++ ※Stock.h
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef Sock_h
+#define Sock_h
 #include <iostream>
 #include <string>
 #include <map>
@@ -14,34 +16,33 @@ protected:
 	std::string EndTime;
 	double EPSactual;
 	double EPSestimate;
+	std::map<Date, double> Prices;
+	std::map<Date, double> Returns;
 
 
 public:
-	std::map<Date, double> Prices;
-	std::map<Date, double> Returns;
 
 	Stock() {}
 	Stock(std::string ticker, std::string starttime, std::string endtime, double eps_act, double eps_est)
 		:Ticker(ticker), StartTime(starttime), EndTime(endtime), EPSactual(eps_act), EPSestimate(eps_est) {}
 	Stock(const Stock& stock) :Ticker(stock.Ticker), StartTime(stock.StartTime), EndTime(stock.EndTime),
-		EPSactual(stock.EPSactual), EPSestimate(stock.EPSestimate) {}
+		EPSactual(stock.EPSactual), EPSestimate(stock.EPSestimate), Prices(stock.Prices), Returns(stock.Returns) {}
 	virtual ~Stock() {} /*constructer uses 5 protected arguments rather than prices and returns*/
 
+	void setPrices(const Date& date, const double& price) { Prices.insert(std::pair<Date, double>(date, price)); }
+	void setReturns(const Date& date, const double& returna) { Prices.insert(std::pair<Date, double>(date, returna)); }
 	std::string getTicker() const { return Ticker; }
 	std::string getStartTime() { return StartTime; }
 	std::string getEndTime() { return EndTime; }
 	double getEPSactual() { return EPSactual; }
 	double getEPSestimate() { return EPSestimate; }
-
+	double getEPSbeat() { return (EPSactual / EPSestimate) - 1; }	
+	std::map<Date, double> GetReturns() const { return Returns; }
 	void setReturns();
 	double getReturns(int t) { auto it = Returns.begin(); advance(it, t); return it->second; }
-	/*double getPrices(int t);
-	double getReturns(int t);
-	const std::map<Date, double, CmpByKeyDate> GetPrices() const { return Prices; }
-	const std::map<Date, double, CmpByKeyDate> GetReturns() const { return Returns; }*/
-	/*Currently we dont need these functions because prices and returns are public*/
+	double getPrices(int t) { auto it = Prices.begin(); advance(it, t); return it->second; }
 
-	double getEPSbeat() const { return (EPSactual / EPSestimate) - 1; }
+	/*Currently we dont need these functions because prices and returns are public*/
 	void Display();
 };
 
@@ -53,7 +54,7 @@ public:
 	Market(const Market& market) :Stock(market) {}
 	void slice(Date startdate, Date enddate, Market* market);
 };
-
+#endif
 
 /*
 class Information : public Stock {
