3967b17b55566193b4dfff9783c4022fa2018799
hiiideee <36652278+hiiideee@users.noreply.github.com>
2018-04-30 12:30:58 -0400

    Update and rename TickerBook.h to ※TickerBook.h

--- ※TickerBook.h
+++ ※TickerBook.h
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef TickerBook_h
+#define TickerBook_h
 #include <iostream>
 #include <string>
 #include <map>
@@ -10,8 +12,6 @@
 
 class TickerBook {
 private:
-	//int Size;
-	//int RequiredSize;
 
 public:
 	std::map<std::string, Stock> Book;
@@ -20,21 +20,24 @@ public:
 	std::vector<double> CAAR = std::vector<double>(90);
 
 	TickerBook() {}
-	TickerBook(const TickerBook& tickerbook): Book(tickerbook.Book), BookPage(tickerbook.BookPage){}
+	TickerBook(const TickerBook& tickerbook) : Book(tickerbook.Book), BookPage(tickerbook.BookPage) {}
 	~TickerBook() {}
 
-
+	//std::map<std::string, Stock> getBook() { return Book; }
+	//std::vector<std::string> getBookPage() { return BookPage; }
 	//void setStock(const Stock& stock);
 	//void setRequiredSize(int R) { RequiredSize = R; };
 	//int getSize() const { return Size; };
 	//void reorder();
+	void setBook(const std::string &ticker, const Stock &stock) { Book.insert(std::pair<std::string, Stock>(ticker, stock)); }
+	void setBookPage(const std::string &ticker) { BookPage.push_back(ticker); }
 	std::vector<double > GetAAR() const { return AAR; }
 	std::vector<double> GetCAAR() const { return CAAR; }
 	int getsize() const { return BookPage.size(); }
-	//std::vector<std::string> Divide(double threshold, int number);
 	void Divide(double threshold, TickerBook *group1, TickerBook *group2, TickerBook *group3);
 	//void display();
-	bool Compute(Market market);
+	bool Compute(Market *market);
 };
 
+#endif
 
