a47c6def8d68db12cbc023a6ee1d0a58058ce1a6
hiiideee <36652278+hiiideee@users.noreply.github.com>
2018-04-30 12:27:41 -0400

    Update and rename BootStrapping.cpp to ※BootStrapping.cpp

--- ※BootStrapping.cpp
+++ ※BootStrapping.cpp
@@ -20,33 +20,28 @@ void swap(int *a, int *b) {
 	return;
 }
 
-void Bootstrapping::run(Market market, TickerBook tickerbook) {
+void Bootstrapping::run(Market *market, TickerBook *tickerbook) {
 	srand(time(NULL));
-	for (int i = 0; i <= Resampletime; i++) {
-		int a = tickerbook.getsize();
+	for (int i = 0; i < Resampletime; i++) {
+		int a = tickerbook->getsize();
 		vector<int> rs = vector<int>(a);
 		for (int j = 0; j < a; ++j) rs[j] = j;
 		for (int j = a - 1; j >= 1; --j) swap(rs[j], rs[rand() % j]);
-		cout << "yeah" << endl;
 		TickerBook newgroup;
-		vector<string> newmap;
-		cout << "yeah" << endl;
 		for (int j = 0; j < 60; j++) {
-			vector<string>::iterator it = tickerbook.BookPage.begin();
+			vector<string>::iterator it = tickerbook->BookPage.begin();
 			advance(it, rs[j]);
-			newmap.push_back(*it);
+			auto itr = tickerbook->Book.find(*it);
+			newgroup.setBook(itr->first, itr->second);
+			newgroup.setBookPage(*it);
 		}
-		cout << "yeah" << endl;
-		newgroup.BookPage = newmap;
-		cout << "yeah" << endl;
 		if (newgroup.Compute(market)) {
-			cout << "yeah" << endl;
 			for (int t = 0; t < 90; t++) {
 				AARlist[t] = AARlist[t] * i / (i + 1) + newgroup.GetAAR()[t] / (i + 1);
 				CAARlist[t] = CAARlist[t] * i / (i + 1) + newgroup.GetCAAR()[t] / (i + 1);
 			}
 		}
-		cout << "yeah" << endl;
+		cout << "Success " << i + 1 << " Times" << endl;
 	}
 	return;
 }
